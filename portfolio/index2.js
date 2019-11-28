dfe7c0ac35566fe1bd45f925f6c6faa837ddb3fc
ahmed <57170057+ahmedelmou@users.noreply.github.com>
2019-11-29 00:45:38 +0200

    29.11.2019

--- portfolio/index2.js
+++ portfolio/index2.js
@@ -1,4 +1,9 @@
-function myFuntion(){
-var d = new Date();
-document.getElementById("myFuntion").innerHTML = d;
+
+function myFunction() {
+    location.href = "index.html";
 }
+$(document).ready(function(){
+    $("th3").click(function(){
+      $("#kuva1").slideToggle(1000);
+    });
+});
